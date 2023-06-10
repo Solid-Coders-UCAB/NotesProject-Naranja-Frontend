46880a9bf3feb9c8ca9aace4b55b5fc22e93388a
dfrojas20 <123292688+dfrojas20@users.noreply.github.com>
2023-06-10 12:25:52 -0400

    cambio de dev -> dependencies

---
+++ notasFrontNaranja/lib/application/getImageFromCameraService.dart
@@ -0,0 +1,31 @@
+// ignore: camel_case_types
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+abstract class imagePicker {
+  Future<File> getImage();
+}
+
+class getImageFromCamaraService {
+  imagePicker? _picker;
+  
+}
+
+class imagePickerImp implements imagePicker{
+  
+  @override
+  Future<File> getImage() async {
+      
+      PickedFile? pickedImage = await ImagePicker().getImage(
+      source: ImageSource.camera ,
+      imageQuality: 50);
+
+      if (pickedImage != null){
+        PickedFile picked = pickedImage;
+        return File(picked.path);
+      }
+
+     return File('');
+  }
+  
+}
