a9a1e8472191e22cbbb2a1cb94777d360ceb3cad
JianLiu08 <129233285+JianLiu08@users.noreply.github.com>
2023-07-16 19:33:01 -0400

    Cambio a la interfaz
    Cambio a la interfaz de Inicio de Sesión

--- notasFrontNaranja/lib/infrastructure/views/systemWidgets/widgets.dart
+++ notasFrontNaranja/lib/infrastructure/views/systemWidgets/widgets.dart
@@ -7,14 +7,15 @@ import 'package:firstapp/infrastructure/views/noteWidgets/speech_to_text_prueba.
 // Widgets que se pueden reutilizar en varias interfaces
 
 // Campo para escribir texto en una sola linea
-Widget genericTextFormField(TextEditingController controllerData, String text, boolean,int maxLength) {
+Widget genericTextFormField(
+    TextEditingController controllerData, String text, boolean, int maxLength) {
   return Container(
     padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
     child: TextFormField(
       controller: controllerData,
       maxLength: maxLength,
       style: const TextStyle(
-      color: Color.fromARGB(255, 154, 181, 255), 
+        color: Color.fromARGB(255, 0, 10, 147),
       ),
       obscureText: boolean,
       decoration: InputDecoration(
@@ -31,10 +32,10 @@ Widget genericTextFormField(TextEditingController controllerData, String text, b
         //    color: Color.fromARGB(255, 154, 18, 255),
         //    width: 2,
         //  ),
-       // ),
+        // ),
         labelText: text,
         labelStyle: const TextStyle(
-          color: Color.fromARGB(255, 154, 181, 255), 
+          color: Color.fromARGB(255, 90, 184, 255),
         ),
       ),
     ),
@@ -42,7 +43,8 @@ Widget genericTextFormField(TextEditingController controllerData, String text, b
 }
 
 // Campo para escribir texto en varias lineas
-Widget maxLinesTextFormField(TextEditingController controllerData, String text, boolean,int maxLength) {
+Widget maxLinesTextFormField(
+    TextEditingController controllerData, String text, boolean, int maxLength) {
   return Container(
     padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
     child: TextFormField(
@@ -50,7 +52,7 @@ Widget maxLinesTextFormField(TextEditingController controllerData, String text, 
       maxLength: maxLength,
       maxLines: null,
       style: const TextStyle(
-      color: Color.fromARGB(255, 154, 181, 255), 
+        color: Color.fromARGB(255, 154, 181, 255),
       ),
       obscureText: boolean,
       decoration: InputDecoration(
@@ -70,7 +72,7 @@ Widget maxLinesTextFormField(TextEditingController controllerData, String text, 
         ),
         labelText: text,
         labelStyle: const TextStyle(
-          color: Color.fromARGB(255, 154, 181, 255), 
+          color: Color.fromARGB(255, 154, 181, 255),
         ),
       ),
     ),
@@ -84,13 +86,14 @@ Widget genericSizedBox(double size) {
 }
 
 Widget iconApp(double sizeIcon) {
-  return Image.network(
-    "https://cdn-icons-png.flaticon.com/512/1001/1001259.png",
-    width: sizeIcon
-  );
+  return Image.asset('assets/images/LogoSolo 1.png');
+  // return Image.network(
+  //   "https://cdn-icons-png.flaticon.com/512/1001/1001259.png",
+  //   width: sizeIcon
+  // );
 }
 
-Widget textLabel(String textInput,double fontSize) {
+Widget textLabel(String textInput, double fontSize) {
   return Text(
     textInput,
     style: TextStyle(
@@ -98,7 +101,7 @@ Widget textLabel(String textInput,double fontSize) {
   );
 }
 
-void alerta(BuildContext context, bool isGood,String title, String content) {
+void alerta(BuildContext context, bool isGood, String title, String content) {
   IconData icon = isGood ? Icons.add_task_rounded : Icons.cancel_outlined;
   Color color = isGood ? Colors.green : Colors.red;
 
@@ -129,76 +132,71 @@ void alerta(BuildContext context, bool isGood,String title, String content) {
 }
 
 // Widget que despliega un menu con las opciones adicionales que se pueden realizar en la nota (imagen a texto, esbozar y voz a texto)
-Widget opcionesNota( NuevaNotaState state){
-  
- return PopupMenuButton(
-  
-  color: Colors.blue,
-  icon: const Icon(Icons.add),
+Widget opcionesNota(NuevaNotaState state) {
+  return PopupMenuButton(
+      color: Colors.blue,
+      icon: const Icon(Icons.add),
+      itemBuilder: (BuildContext context) => <PopupMenuItem>[
+            PopupMenuItem(
+                child: Container(
+              child: Wrap(
+                direction: Axis.horizontal,
+                children: <Widget>[
+                  // Opcion de imagen a texto
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: InkWell(
+                      child: const Icon(Icons.camera_alt),
+                      onTap: () {
+                        state.getTextFromIa();
+                      },
+                    ),
+                  ),
 
-  itemBuilder: (BuildContext context) =><PopupMenuItem>[
-    PopupMenuItem(
-      child: Container(
-        child:  Wrap(
-          direction: Axis.horizontal,
-          children: <Widget>[
-            
-            // Opcion de imagen a texto
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: InkWell(
-                child: const Icon(Icons.camera_alt),
-                onTap: (){
-                    state.getTextFromIa();
-                },
-                ),
+                  // Opcion de esbozar una nota
+                  Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: InkWell(
+                        child: const Icon(Icons.draw),
+                        onTap: () async {
+                          Uint8List? imagen = await Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const DrawingRoomScreen()));
+                          state.getEsbozado(imagen);
+                        },
+                      )),
+
+                  // Opcion de voz a texto
+                  Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: InkWell(
+                        child: const Icon(Icons.mic),
+                        onTap: () async {
+                          String espacio = " ";
+                          String audio = await Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      SpeechScreen(text: '')));
+                          state.showAudioToText(
+                              contenido: state.getContenido() + audio + espacio,
+                              titulo: state.getTitulo());
+                        },
+                      )),
+
+                  // Opcion de elegir imagen de la galeria
+                  Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: InkWell(
+                        child: const Icon(Icons.photo),
+                        onTap: () {
+                          state.getFromGallery();
+                        },
+                      )),
+                ],
               ),
-
-            // Opcion de esbozar una nota
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: InkWell(
-                child: const Icon(Icons.draw),
-                onTap: () async {
-                  Uint8List? imagen = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const DrawingRoomScreen()));
-                  state.getEsbozado(imagen);
-
-                },
-                )
-            ),
-
-            // Opcion de voz a texto
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: InkWell(
-                child: const Icon(Icons.mic),
-                onTap: () async {            
-                  String espacio = " ";
-                   String audio = await Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) =>  SpeechScreen(text: '')));                    
-                    state.showAudioToText(contenido: state.getContenido() + audio + espacio,titulo: state.getTitulo() );
-                },
-                )
-            ),
-
-            // Opcion de elegir imagen de la galeria
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: InkWell(
-                child: const Icon(Icons.photo),
-                onTap: ()  {            
-                    state.getFromGallery();
-                },
-                )
-            ),
-          ],
-        ),
-      )
-    )
-  ]
-  );
+            ))
+          ]);
 }
